f7cf1c46a9120249af91496932509040f783b85a
Shariare tanvir akash <shariaretanvir_akash@yahoo.com>
2024-03-29 20:43:28 +0000

    add test project

---
+++ StudentApp.Tests/Handlers/Student/GetStudentHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using Castle.Core.Logging;
+using MapsterMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using StudentApp.Core.Common;
+using StudentApp.Core.Feature.Student.Get;
+using StudentApp.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudentApp.Tests.Handlers.Student
+{
+    public class GetStudentHandlerTests
+    {
+        private readonly Mock<ILogger<GetStudentHandler>> _logger;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IRepository> _repository;
+
+        public GetStudentHandlerTests()
+        {
+            _logger = new Mock<ILogger<GetStudentHandler>>();
+            _mapper = new Mock<IMapper>();
+            _repository = new Mock<IRepository>();
+        }
+        [Fact]
+        public async void GetStudentHandler_Handle_GetStudentResponse()
+        {
+            //arrange
+            GetStudentHandler handler = new GetStudentHandler(_logger.Object, _repository.Object, _mapper.Object);
+            var resourceParamMock = new ResourceParameters { PageNumber = 1, PageSize = 5 };
+            var request = new GetStudentRequest { ResourceParameters = resourceParamMock };
+
+            var response = new GetStudentResponse(new List<GetStudentModel>(), new PaginationMetaData());
+            var studentObject = new List<Domain.Entities.Student>
+            {
+                new Domain.Entities.Student(Guid.NewGuid(), "Akash",  30, true),
+            };
+            var pagedList = PagedList<Entities.Student>.Create(studentObject, 1, 5);
+            var mapedData = _repository.Setup(x => x.GetAll(request.ResourceParameters)).ReturnsAsync(pagedList);
+
+            var mocStudentModel = new List<GetStudentModel>();
+            var studentModel = _mapper.Setup(x => x.Map<List<GetStudentModel>>(pagedList.Items)).Returns(new List<GetStudentModel>());
+            var paginMetaDataMock = _mapper.Setup(c => c.Map<PaginationMetaData>(pagedList)).Returns(new PaginationMetaData { CurrentPage = 1, PageSize = 5 });
+
+            CancellationToken token = CancellationToken.None;
+            //act
+            var result = await handler.Handle(request, token);
+
+            //arrange
+
+            Assert.NotNull(result);
+            Assert.Equal(5, result.PaginationMetaData.PageSize = 5);
+        }
+    }
+}
---
+++ StudentApp.Tests/Handlers/Student/PostStudentTests.cs
@@ -0,0 +1,69 @@
+﻿using MapsterMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using StudentApp.Core.Feature.Student.Get;
+using StudentApp.Core.Feature.Student.Post;
+using StudentApp.Core.Infra;
+using StudentApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp.Tests.Handlers.Student
+{
+    public class PostStudentTests
+    {
+        private readonly Mock<ILogger<PostStudentHandler>> _logger;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        public PostStudentTests()
+        {
+            _logger = new Mock<ILogger<PostStudentHandler>>();
+            _mapper = new Mock<IMapper>();
+            _unitOfWork = new Mock<IUnitOfWork>();
+        }
+        [Fact]
+        public async void PostStudentHandler_Handle_PostStudentResponse()
+        {
+            //arrange
+            var studentId = Guid.NewGuid();
+            var postHandler = new PostStudentHandler(_mapper.Object, _unitOfWork.Object);
+            var studentDtoMock = new PostStudentCommand { StudentDetailsModels = new List<StudentAddressModel> { new StudentAddressModel { Id = Guid.NewGuid(), AddressType = AddressType.Permanent, FullAddress = "Birmingham", StudentId = studentId } } };
+            var studentMock = new Entities.Student(studentId, "Akash", 30, true);
+
+            _mapper.Setup(x => x.Map<Entities.Student>(studentDtoMock)).Returns(studentMock);
+
+            var studentRepositoryMock = new Mock<IGenericRepository<Entities.Student, Guid>>();
+            _unitOfWork.Setup(x => x.GetRepository<Entities.Student, Guid>()).Returns(studentRepositoryMock.Object);
+            studentRepositoryMock.Setup(x => x.AddAsync(studentMock)).Returns(Task.CompletedTask);
+
+            if (studentDtoMock.StudentDetailsModels.Count > 0)
+            {
+                //var studentAddressDtoMock = new Mock<List<StudentAddressModel>>();
+                var studentAddressMock = new Mock<List<Entities.StudentAddress>>();
+                studentAddressMock.Object.Add(new Domain.Entities.StudentAddress(Guid.NewGuid(), AddressType.Permanent, "Akash"));
+                                
+                _mapper.Setup(x => x.Map<List<Entities.StudentAddress>>(studentDtoMock.StudentDetailsModels)).Returns(studentAddressMock.Object);
+                studentAddressMock.Object.ForEach(x => x.SetStudent(studentMock));
+
+                var addressRepository = new Mock<IGenericRepository<Entities.StudentAddress, Guid>>();
+                _unitOfWork.Setup(x => x.GetRepository<Entities.StudentAddress, Guid>()).Returns(addressRepository.Object);
+                addressRepository.Setup(x => x.AddRangeAsync(studentAddressMock.Object)).Returns(Task.CompletedTask);
+            }
+            CancellationToken token = CancellationToken.None;
+            _unitOfWork.Setup(x => x.CommitAsync(token)).ReturnsAsync(1);
+
+            //var addStudent = _unitOfWork.Setup(x => x.GetRepository<Entities.Student, Guid>().AddAsync(studentMock.Object)).Returns();
+
+            //act
+            var response = await postHandler.Handle(studentDtoMock, token);
+
+            //assert
+            Assert.NotNull(response);
+
+        }
+    }
+}
